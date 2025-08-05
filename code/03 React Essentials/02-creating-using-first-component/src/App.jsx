5e0e739039de061aa7bd24f80b2d288e4a33d44f
Quentin Quarles <quentina@yamaha-robotics.com>
2025-08-05 13:01:33 +0900

    Completed Section 3, Step 44.

--- code/03 React Essentials/02-creating-using-first-component/src/App.jsx
+++ code/03 React Essentials/02-creating-using-first-component/src/App.jsx
@@ -1,6 +1,7 @@
-import reactImage from './assets/react-core-concepts.png';
+import reactImage from "./assets/react-core-concepts.png";
+import componentImg from './assets/components.png';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -10,7 +11,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
 
   return (
-    <header>      
+    <header>
       <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
@@ -21,12 +22,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
