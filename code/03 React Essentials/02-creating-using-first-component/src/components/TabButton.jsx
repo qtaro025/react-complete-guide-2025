52e84a60b459da40aeaf41a7d080eae29fdaf0bc
Quentin Quarles <qtaro025@gmail.com>
2025-08-07 21:00:22 +0900

    Completed Section 3 Step 50 - Reacting to Events [Core Concept].

--- code/03 React Essentials/02-creating-using-first-component/src/components/TabButton.jsx
+++ code/03 React Essentials/02-creating-using-first-component/src/components/TabButton.jsx
@@ -1,7 +1,11 @@
-export default function TabButton({children}) {
-    return (
-        <li>
-            <button>{children}</button>
-        </li>
-    )
+export default function TabButton({ children }) {
+  function handleClick() {
+    console.log("Hello World!");
+  }
+
+  return (
+    <li>
+      <button onClick={handleClick}>{children}</button>
+    </li>
+  );
 }
